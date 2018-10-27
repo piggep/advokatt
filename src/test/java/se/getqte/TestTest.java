c4093c12f8e51d6a867d669778400803d588da24
rahlenoskar@gmail.com <rahlenoskar@gmail.com>
2018-10-27 12:12:39 +0200

    fixed the property source for testing

--- src/test/java/se/getqte/TestTest.java
+++ src/test/java/se/getqte/TestTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import se.getqte.model.repository.ClientRepository;
 import se.getqte.tenant.TenantContext;
@@ -17,6 +18,7 @@ import java.util.logging.Logger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:test.properties")
 public class TestTest {
 
     @Autowired
